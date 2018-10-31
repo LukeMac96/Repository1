2b7a01ab869e24c28d7dcf939f41938ba653a366
luke <luke@lukes-mbp.nuigalway.ie>
2018-10-31 15:29:47 +0000

    AppTest Junit edit

--- Assignment_1/src/test/java/CT_417/Assignment_1/AppTest.java
+++ Assignment_1/src/test/java/CT_417/Assignment_1/AppTest.java
@@ -7,32 +7,15 @@ import junit.framework.TestSuite;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+public class AppTest extends TestCase { 
+	 
+		public void testGetUsername() {
+			
+			student dave = new student("Dave", 22, "04/11/1996"/*, 15463611*/);
+			
+			assertEquals(dave.getUsername(), "Dave22");
+			
+			
+			//fail("Not yet implemented");
     }
 }
